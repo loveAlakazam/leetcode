b62e3d3ce6ab144448b613daa2a4f48aeb644b1c
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-03-20 16:28:43 +0900

    Update MinStack.py

--- 155/MinStack.py
+++ 155/MinStack.py
@@ -1,5 +1,5 @@
 class MinStack:
-
+    # MinStack 클래스 생성자
     def __init__(self):
         """
         initialize your data structure here.
@@ -8,21 +8,20 @@ class MinStack:
         self.stack = []
         
 
-    #원소 x를 넣는다.
+    #메소드 push :원소 x를 넣는다.
     def push(self, x: int) -> None:
         self.stack.append(x)
   
-    #맨위의 원소를 뺀다.
+    #메소드 pop: 맨위의 원소를 뺀다.
     def pop(self) -> None:
         if len(self.stack) >0:
             self.stack.pop()
         
-    #맨위의 원소를 빼지 않고 값만 확인
+    #메소드 top: 맨위의 원소를 빼지 않고 값만 확인
     def top(self) -> int:
         if len(self.stack)>0:
             return self.stack[-1]
-        
-
+    #메소드 getMin :스택의 원소들 중 가장작은 원소를 찾는다.
     def getMin(self) -> int:
         if len(self.stack)>0:
             return min(self.stack)
