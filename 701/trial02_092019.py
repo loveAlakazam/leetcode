be40114f06962bd8a454bb5a0b4d405997d274fc
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-09-20 16:57:33 +0900

    Update trial02_092019.py

--- 701/trial02_092019.py
+++ 701/trial02_092019.py
@@ -12,17 +12,17 @@ class Solution(object):
         :type val: int
         :rtype: TreeNode
         """
-        def insertion(pre,root,key,q):
-            if root is None and pre is None:#비어있는 트리
+        def insertion(pre,now,key,origin_root):
+            if now is None and pre is None:#비어있는 트리
                 return TreeNode(key) #새로운 노드를 만들어서 리턴.
             
             else:
-                if root:
-                    pre=root
-                    if root.val>key:
-                        return insertion(pre,root.left,key,q)
-                    else:#root.val<key
-                        return insertion(pre,root.right,key,q)
+                if now:
+                    pre=now
+                    if now.val>key:
+                        return insertion(pre,now.left,key,origin_root)
+                    else:# now.val<key
+                        return insertion(pre,now.right,key,origin_root)
                 
                 else:#root 가 None pre(선행노드)의 값과 비교하여 왼쪽/오른쪽 자식으로 선택=> 노드생성해서 삽입
                     if pre.val>key:
@@ -32,7 +32,7 @@ class Solution(object):
                     else:#pre.val<key
                         #노드를 생성해서 넣는다.
                         pre.right=TreeNode(key)
-            return q #q는 root원본이다.
-        p,q=root,root
-        x=insertion(None,p,val,q)
+            return origin_root # origin_root는 root원본이다.
+        now,origin_root=root,root
+        x=insertion(None,now,val,origin_root)
         return x
